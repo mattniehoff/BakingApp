0dbd2ba24d325aa7b308838f6733d595eca4dafa
Matt Niehoff <nieho003@umn.edu>
2018-10-06 13:35:02 -0500

    Initial commit. Defines json model.

---
+++ app/src/main/java/com/mattniehoff/bakingapp/model/RecipesResponse.java
@@ -0,0 +1,144 @@
+package com.mattniehoff.bakingapp.model;
+
+import java.util.List;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.os.Parcelable.Creator;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class RecipesResponse implements Parcelable
+{
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("ingredients")
+    @Expose
+    private List<Ingredient> ingredients = null;
+    @SerializedName("steps")
+    @Expose
+    private List<Step> steps = null;
+    @SerializedName("servings")
+    @Expose
+    private Integer servings;
+    @SerializedName("image")
+    @Expose
+    private String image;
+    public final static Parcelable.Creator<RecipesResponse> CREATOR = new Creator<RecipesResponse>() {
+
+
+        @SuppressWarnings({
+                "unchecked"
+        })
+        public RecipesResponse createFromParcel(Parcel in) {
+            return new RecipesResponse(in);
+        }
+
+        public RecipesResponse[] newArray(int size) {
+            return (new RecipesResponse[size]);
+        }
+
+    }
+            ;
+
+    protected RecipesResponse(Parcel in) {
+        this.id = ((Integer) in.readValue((Integer.class.getClassLoader())));
+        this.name = ((String) in.readValue((String.class.getClassLoader())));
+        in.readList(this.ingredients, (com.mattniehoff.bakingapp.model.Ingredient.class.getClassLoader()));
+        in.readList(this.steps, (com.mattniehoff.bakingapp.model.Step.class.getClassLoader()));
+        this.servings = ((Integer) in.readValue((Integer.class.getClassLoader())));
+        this.image = ((String) in.readValue((String.class.getClassLoader())));
+    }
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public RecipesResponse() {
+    }
+
+    /**
+     *
+     * @param ingredients
+     * @param id
+     * @param servings
+     * @param name
+     * @param image
+     * @param steps
+     */
+    public RecipesResponse(Integer id, String name, List<Ingredient> ingredients, List<Step> steps, Integer servings, String image) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.steps = steps;
+        this.servings = servings;
+        this.image = image;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<Step> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<Step> steps) {
+        this.steps = steps;
+    }
+
+    public Integer getServings() {
+        return servings;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(id);
+        dest.writeValue(name);
+        dest.writeList(ingredients);
+        dest.writeList(steps);
+        dest.writeValue(servings);
+        dest.writeValue(image);
+    }
+
+    public int describeContents() {
+        return 0;
+    }
+
+}
