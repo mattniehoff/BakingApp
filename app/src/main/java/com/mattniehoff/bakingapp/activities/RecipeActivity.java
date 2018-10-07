7142eb8ca564eae3150caad7f5d23f5ef90af8d7
Matt Niehoff <matt.j.niehoff@gmail.com>
2018-10-07 11:06:05 -0500

    Removes/comments out unused fabs

--- app/src/main/java/com/mattniehoff/bakingapp/activities/RecipeActivity.java
+++ app/src/main/java/com/mattniehoff/bakingapp/activities/RecipeActivity.java
@@ -47,20 +47,22 @@ public class RecipeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_step_list);
 
         Bundle data = getIntent().getExtras();
-        Recipe recipe = data.getParcelable(RECIPE_EXTRA);
+        if (data != null) {
+            Recipe recipe = data.getParcelable(RECIPE_EXTRA);
+        }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         if (findViewById(R.id.step_detail_container) != null) {
             // The detail container view will be present only in the
